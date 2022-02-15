db0812096f0c33d82d9c64d370fa198262e93420
fuzewei <404283574@qq.com>
2022-02-15 21:07:41 +0800

    editor

--- Assets/Editor/Ui/EditorPanel.cs
+++ Assets/Editor/Ui/EditorPanel.cs
@@ -9,8 +9,30 @@ public class EditorPanel
     [MenuItem("Tools/从Start场景开始Play")]
     private static void rePlay()
     {
-        EditorApplication.ExitPlaymode();
-        EditorSceneManager.OpenScene("Assets/start.unity");
-        EditorApplication.EnterPlaymode();
+        if (EditorApplication.isPlaying)
+        {
+            EditorApplication.ExitPlaymode();
+        }
+        EditorApplication.update += rePlayCheck;
     }
+
+    #region rePlayCheck
+    private static void rePlayCheck()
+    {
+        if (
+            EditorApplication.isPlaying || EditorApplication.isPaused ||
+            EditorApplication.isCompiling || EditorApplication.isPlayingOrWillChangePlaymode)
+        {
+            return;
+        }
+        EditorApplication.update -= rePlayCheck;
+
+        if (EditorSceneManager.SaveCurrentModifiedScenesIfUserWantsTo())
+        {
+            EditorSceneManager.OpenScene("Assets/start.unity");
+            EditorApplication.EnterPlaymode();
+        }
+
+    }
+    #endregion
 }
